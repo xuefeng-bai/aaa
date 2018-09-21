f72cf9487812b47899f853a5d787a7422575224a
bxf <17600542215@163.com>
2018-09-21 16:53:29 +0800

    增加了一个系统输出

--- src/main/java/com/example/demosink/AAA.java
+++ src/main/java/com/example/demosink/AAA.java
@@ -1,8 +1,13 @@
 package com.example.demosink;
 
+import java.util.Scanner;
+
 public class AAA {
 	public static void main(String[] args) {
 		System.out.println("11111");
 		System.out.println("-------");
+
+		
+		System.out.println("000000000000000000");
 	}
 }
